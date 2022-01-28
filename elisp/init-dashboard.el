04f0e8b07e224515a217a4af1190fb69186d9adc
gonglongfei <2239660080@qq.com>
2022-01-29 02:54:04 +0800

    fix

--- elisp/init-dashboard.el
+++ elisp/init-dashboard.el
@@ -7,7 +7,7 @@
   (setq dashboard-set-init-info t)
   (setq dashboard-init-info "Highlord Mograine . The Ashbringer") ;; banner 下文字, 不设置就显示加载时间
   (setq dashboard-set-footer nil) ;; 底部文字
-  (setq dashboard-startup-banner "~/.emacs.d/banner/text1.txt") ;; banner
+  (setq dashboard-startup-banner "~/.emacs.d/banner/magic1.gif") ;; banner
   ; (setq dashboard-center-content t) ;; 选项居中
 )
 
