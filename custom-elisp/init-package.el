f743a9db69f8e8959e770c4ac96a0ac5d06b0270
gonglongfei <2239660080@qq.com>
2022-12-06 19:04:23 +0800

    fix

--- custom-elisp/init-package.el
+++ custom-elisp/init-package.el
@@ -3,6 +3,7 @@
 ;; ----------------------------------------------
 (setq package-check-signature nil) ; 禁用签名校验
 (setq package-archives '(
+    ;; ("elpy" . "https://jorgenschaefer.github.io/packages/")
     ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
     ("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
     ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/"))
@@ -22,14 +23,14 @@
 (unless (package-installed-p 'use-package) 
     (package-refresh-contents) 
     (package-install 'use-package))
-
 ;; use-package 全局设置, 安装时不用每次都设置
 (eval-and-compile 
     (setq use-package-always-ensure t) ; 确保一定安装
     (setq use-package-always-defer t) ; 延迟加载
     (setq use-package-always-demand nil) 
     (setq use-package-expand-minimally t) 
-    (setq use-package-verbose t))
+    (setq use-package-verbose t)
+)
 
 
 (provide 'init-package)
--- init.el
+++ init.el
@@ -17,6 +17,10 @@
 ;; (require 'init-lsp-python)
 ;; (require 'init-lsp-java)
 
+
+(require 'init-elpy) ; python 开发环境
+
+
 ;; ;; Change window
 ;; (require 'init-window)
 
@@ -62,3 +66,17 @@
 
 ;; ;; keymapping
 ;; (require 'init-keymapping)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(elpy which-key use-package undo-tree neotree lsp-ui lsp-ivy gruvbox-theme flycheck doom-modeline dashboard dap-mode benchmark-init all-the-icons))
+ '(warning-suppress-log-types '(((flymake flymake)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
