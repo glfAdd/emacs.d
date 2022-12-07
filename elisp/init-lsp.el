b75f8446e47f35a8df62fb4fd6f13006d276e435
gonglongfei <2239660080@qq.com>
2022-12-08 01:12:40 +0800

    success python lps company

--- elisp/init-lsp.el
+++ elisp/init-lsp.el
@@ -1,45 +1,66 @@
-;; python 自动
+
 (setq lsp-pyls-server-command "~/.local/bin/pyls")			
 (setq lsp-pylsp-server-command "~/.local/bin/pylsp")			
 
 
-;; ----------------------------------------------
-;; lsp: 集成代码开发环境
-;; ----------------------------------------------
+(use-package company
+  :ensure t
+  :init (global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
+  (setq company-tooltip-align-annotations t)
+  (setq company-idle-delay 0.0)
+  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
+  (setq company-selection-wrap-around t)
+  (setq company-transformers '(company-sort-by-occurrence))) ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
+
+
+(use-package company-box
+  :ensure t
+  :if window-system
+  :hook (company-mode . company-box-mode))
+
+
+
+;; 语法检测
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+
+
 (use-package lsp-mode
-    :init
-    ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-    (setq lsp-keymap-prefix "C-c l")
-    :hook (
-        (python-mode . lsp-deferred) ; 表示启用 python-mode 时调用 lsp, 延迟加载
-        ;; if you want which-key integration
-        (lsp-mode . lsp-enable-which-key-integration)
-    )
-    :commands
-    (lsp lsp-deferred)
-)
+  :ensure t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l"
+	lsp-file-watch-threshold 500)
+  :hook 
+  (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
+  :commands (lsp lsp-deferred)
+  :config
+    (setq lsp-completion-provider :none) ;; 阻止 lsp 重新设置 company-backend 而覆盖我们 yasnippet 的设置
+    (setq lsp-headerline-breadcrumb-enable t))
 
-
-;; ----------------------------------------------
-;; 集成 flycheck 和更高级别的 UI 模块
-;; ----------------------------------------------
 (use-package lsp-ui
-    :init
-    :commands lsp-ui-mode
-)
+  :ensure t
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  (setq lsp-ui-doc-position 'top))
 
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-(use-package which-key
-    :config
-    (which-key-mode))
+(use-package lsp-ivy
+  :ensure t
+  :after (lsp-mode))
 
 
-;; ----------------------------------------------
-;; 代码补全
-;; ----------------------------------------------
+
+(use-package pyvenv
+  :ensure t
+  :config
+  (setenv "WORKON_HOME" "~/.pyenv/versions/p3710-emacs")
+  (setq python-shell-interpreter "python3")
+  (pyvenv-mode t))
 
 
 (provide 'init-lsp)
