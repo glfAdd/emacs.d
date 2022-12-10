4f238e4fca32280be95f2ddc104e06d9d11ec4c0
gonglongfei <2239660080@qq.com>
2022-12-10 23:07:10 +0800

    success

--- elisp/init-lsp.el
+++ elisp/init-lsp.el
@@ -1,59 +1,64 @@
-
-(setq lsp-pyls-server-command "~/.local/bin/pyls")			
-(setq lsp-pylsp-server-command "~/.local/bin/pylsp")			
-
-
+;; ----------------------------------------------
+;; company
+;; ----------------------------------------------
 (use-package company
-  :ensure t
-  :init (global-company-mode)
-  :config
-  (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
-  (setq company-tooltip-align-annotations t)
-  (setq company-idle-delay 0.0)
-  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
-  (setq company-selection-wrap-around t)
-  (setq company-transformers '(company-sort-by-occurrence))) ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
+    :ensure t
+    :init (global-company-mode)
+    :config
+    (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
+    (setq company-tooltip-align-annotations t)
+    (setq company-idle-delay 0.0)
+    (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
+    (setq company-selection-wrap-around t)
+    (setq company-transformers '(company-sort-by-occurrence)) ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
+) 
 
 
 (use-package company-box
-  :ensure t
-  :if window-system
-  :hook (company-mode . company-box-mode))
+    :if window-system
+    :hook (company-mode . company-box-mode)
+)
 
+
+;; ----------------------------------------------
+;; lsp
+;; ----------------------------------------------
+(use-package lsp-mode
+    :init
+    (setq lsp-keymap-prefix "C-c l" lsp-file-watch-threshold 500)
+    :hook 
+    (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
+    :commands
+    lsp lsp-deferred
+    :config
+    (setq lsp-completion-provider :none) ; 阻止 lsp 重新设置 company-backend 而覆盖我们 yasnippet 的设置
+    (setq lsp-headerline-breadcrumb-enable t)
+    (setq lsp-idle-delay 0.6)
+    (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+)
+
+(use-package lsp-ui
+    :config
+    (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+    (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+    (setq lsp-ui-doc-position 'top)
+    (setq lsp-ui-peek-always-show t)
+    (setq lsp-ui-sideline-show-hover t)
+    (setq lsp-ui-doc-enable nil)
+)
+
+
+(use-package lsp-ivy
+    :ensure t
+    :after (lsp-mode)
+)
 
 
 ;; 语法检测
 (use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
-
-
-
-(use-package lsp-mode
-  :ensure t
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l"
-	lsp-file-watch-threshold 500)
-  :hook 
-  (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
-  :commands (lsp lsp-deferred)
-  :config
-    (setq lsp-completion-provider :none) ;; 阻止 lsp 重新设置 company-backend 而覆盖我们 yasnippet 的设置
-    (setq lsp-headerline-breadcrumb-enable t))
-
-(use-package lsp-ui
-  :ensure t
-  :config
-  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-  (setq lsp-ui-doc-position 'top))
-
-(use-package lsp-ivy
-  :ensure t
-  :after (lsp-mode))
-
-
+    :ensure t
+    :init (global-flycheck-mode)
+)
 
 (use-package pyvenv
   :ensure t
